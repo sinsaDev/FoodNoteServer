2df7505ba841b66cf56b03a92e00cec8830d84bc
saouk <honggalra2@gmail.com>
2020-08-02 20:51:14 +0900

    유저 상세정보 업데이트 api 추가

--- app/controllers/api/users_description_controller.rb
+++ app/controllers/api/users_description_controller.rb
@@ -10,6 +10,15 @@ class Api::UsersDescriptionController < Api::ApplicationController
     end
   end
 
+  def update
+    user_description = @user.user_description.update user_description_params
+    if user_description.save
+      render json: ResponseWrap.data_wrap(nil), status: :ok
+    else
+      render json: ResponseWrap.data_wrap(nil, user_description.errors.details), status: :bad_request
+    end
+  end
+
   private
   def user_description_params
     params.permit(:address, :gender, :phone, :grade)
