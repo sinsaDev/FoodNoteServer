da20e29789604ddb7a4959e060b0fcebcf48b976
saouk <honggalra2@gmail.com>
2020-08-04 23:26:27 +0900

    migration file modify

--- db/migrate/20200802115259_change_users_id_to_product.rb
+++ db/migrate/20200802115259_change_users_id_to_product.rb
@@ -1,10 +1,10 @@
 class ChangeUsersIdToProduct < ActiveRecord::Migration[6.0]
   def change
     rename_column :products, :users_id, :user_id
-    change_column_default :products, :deleted, "deleted"
+    change_column_default :products, :deleted, "no"
     change_column_default :products, :status, 0
     change_column_default :product_options, :status, 0
-    change_column_default :product_videos, :deleted, "deleted"
+    change_column_default :product_videos, :deleted, "no"
     Product.reset_column_information
   end
 end
