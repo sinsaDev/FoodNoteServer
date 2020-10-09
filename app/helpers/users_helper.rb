89f772295d97abd3ae5b4b76f1432c3e959d746b
saouk <honggalra2@gmail.com>
2020-10-09 23:19:02 +0900

    유저상태 관련 라벨 이름 변경 및 sns별 라벨 추가

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,7 +8,7 @@ module UsersHelper
     end
   end
 
-  def status_label_badge(user)
+  def label_for_user_status(user)
     if user.present?
       if user.status["not_phone_certified"]
         content_tag(:span, "추가정보필요", class: ["badge bg-warning my-auto"])
@@ -23,4 +23,21 @@ module UsersHelper
       content_tag(:span, "유저정보없음", class: ["badge bg-secondary my-auto"])
     end
   end
+
+ def label_for_sns(user)
+   if user.present?
+     if user.kakao?
+       content_tag(:span, "카카오", class: ["badge bg-warning my-auto"])
+     elsif user.facebook?
+       content_tag(:span, "네이버", class: ["badge bg-primary my-auto"])
+     elsif user.naver?
+       content_tag(:span, "네이버", class: ["badge bg-success my-auto"])
+     else
+       content_tag(:span, "상태없음", class: ["badge bg-secondary my-auto"])
+     end
+   else
+     content_tag(:span, "유저정보없음", class: ["badge bg-secondary my-auto"])
+   end
+ end
+
 end
