84f6817139d82b751800a9e2ba3b7a36abf27d01
saouk <honggalra2@gmail.com>
2020-10-09 21:59:25 +0900

    유저리스트페이지에서 유저의 회원가입상태를 볼 수 있는 badge 추가

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,4 +7,20 @@ module UsersHelper
       "정보 미등록"
     end
   end
+
+  def status_label_badge(user)
+    if user.present?
+      if user.status["not_phone_certified"]
+        content_tag(:span, "추가정보필요", class: ["badge bg-warning my-auto"])
+      elsif user.status["unregister"]
+        content_tag(:span, "회원탈퇴", class: ["badge bg-danger my-auto"])
+      elsif user.status["sign_up"]
+        content_tag(:span, "회원", class: ["badge bg-success my-auto"])
+      else
+        content_tag(:span, "상태없음", class: ["badge bg-secondary my-auto"])
+      end
+    else
+      content_tag(:span, "유저정보없음", class: ["badge bg-secondary my-auto"])
+    end
+  end
 end
