3b490e5b06ac7bda7cd9aaddf40d15dc59050c89
saouk <honggalra2@gmail.com>
2020-10-10 22:42:06 +0900

    네비게이션바 클릭시 활성화 시키도록 업데이트

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def view_nav_link(text, path)
+    option = current_page?(path) ? { class: ["active", "nav-link"] } : { class: ["nav-link"]}
+    content_tag(:a, link_to(text, path, option))
+  end
 end
