75db70f82b72cb122ac9c4104d2fe1ca374ec9c1
saouk <honggalra2@gmail.com>
2020-10-13 11:44:07 +0900

    verify_authenticity_token 코드 추가

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::UsersController < Api::ApplicationController
   #before_action :token_authentication, only: [:create]
-  #skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
 
   def create
     user = User.user_information_update(user_params)
